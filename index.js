e3e0fc23ac541f4128a01b153b63faf219324ef3
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-05-22 21:58:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,37 @@ document.addEventListener("mousemove", (e) => {
     const verticalDistance = distanceFromCenter(buttonBox.y, y, buttonBox.height) // y-axis location from center of the element
     const horizontalOffset  = buttonBox.width /2 + OFFSET //distance to start evading the mouse based on x axis
     const verticalOffset = buttonBox.height / 2 + OFFSET //distance to start evading the mouse based on y axis
+
+    if (Math.abs(horizontalDistance) <= horizontalOffset && Math.abs(verticalDistance) <= verticalOffset) {
+        setButtonPosition(buttonBox.x + horizontalOffset / horizontalDistance * 10,
+        buttonBox.y + verticalOffset / verticalDistance * 10
+    )
+    }
+    
 })
+
+const setButtonPosition = (left, top) => {
+    const windowBox = document.body.getBoundingClientRect()
+    const buttonBox = evilButton.getBoundingClientRect()
+
+    if(distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+    left = windowBox.right - buttonBox.width - OFFSET
+    }
+    if(distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+        left = windowBox.left + OFFSET
+    }
+    if(distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+        top = windowBox.bottom - buttonBox.height - OFFSET
+    }
+    if(distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+    top = windowBox.top + OFFSET
+    }
+    
+    console.log(left)
+    console.log(top)
+    evilButton.style.left = `${left}px`
+    evilButton.style.top = `${top}px`
+}
 
 const distanceFromCenter = (boxPosition, mousePosition, boxSize) => {
     return boxPosition - mousePosition + boxSize / 2
